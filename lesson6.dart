6275abe7084f1cb0dbe09ffda1aebdee94356c2c
Pansherakhmed <aliakhmedd@icloud.com>
2024-04-01 04:24:04 +0600

    finish

---
+++ lesson6.dart
@@ -0,0 +1,77 @@
+void main() {
+  // int season = 1;
+  // if (season == 1) {
+  //   print("Whinter");
+  // } else if (season == 2) {
+  //   print("Autumn");
+  // } else if (season == 3) {
+  //   print("Summer");
+  // } else if (season == 4) {
+  //   print("osen");
+  // } else {
+  //   print("error");
+  // }
+
+  // String result = "";
+
+  // switch (season) {
+  //   case 1:
+  //     result = "Whinter";
+  //     break;
+  //   case 2:
+  //     result = ("Autumn");
+  //     break;
+  //   case 3:
+  //     result = ("Summer");
+  //     break;
+  //   case 4:
+  //     result = ("osen");
+
+  //   default:
+  //     result = ("error");
+  // }
+
+  // print(result);
+
+//   String name = "Aliakhmed";
+//   print(name.contains("a"));
+
+//   if (name.contains("a")) {
+//     print("name contains a");
+//   } else if (name.contains("b")) {
+//     print("name contains b");
+//   } else {
+//     print("name don't have a");
+//   }
+// }
+
+  int number = 10;
+  switch (number) {
+    case 1:
+    case 2:
+    case 12:
+      print("zima");
+      break;
+
+    case >= 3 && <= 5:
+      print("vesna");
+      break;
+
+    case 6:
+    case 7:
+    case 8:
+      print("leto");
+      break;
+
+    case 9:
+    case 10:
+    case 11:
+      print("leto");
+      break;
+
+    default:
+      print("error");
+  }
+
+  
+}
